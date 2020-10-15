79bd807bf8a0da60e0114060fabc257ba4e26395
alexandracarmen <61453905+alexandracarmen@users.noreply.github.com>
2020-10-15 15:25:58 +0300

    Update Covid.scala

--- Covid.scala
+++ Covid.scala
@@ -7,7 +7,10 @@ import com.cra.figaro.library.compound_
 object Covid{
 
     private val Covid = Flip (2)
-
+ 
+  val febra= Flip(0.01)
+  val tuse =Flip(0.1)
+    
   val aiCovid =CPD (febra, tuse,
      (false, false)-> Fliip(0.02),
      (true, false) -> Flip (0,35),
@@ -17,14 +20,20 @@ object Covid{
   val teInternezi = CPD(aiCovid,
      true ->Flip(0,8),
      false ->Flip (0.2))
-
-  def main(args: Array[String]) {
-       aiCovid.observe(true)
-       val alg = VariableElimination(febra, tuse)
-       alg.start()
-       println("Probability of febra:" + alg.probability(febra, tuse))
-       alg.kill
-
-  } 
-     
+    
+  teInternezi.observe(true)
+   val alg =VariableElimination(febra, tuse)
+   alg.start()
+   println("Probability of febra: "+ alg.probability(febra, true))
+   println("Probability of tuse: "+alg.probability(tuse, true))
+   alg.kill
+    
+   tuse.observe(false)
+   febra.observe(false)
+    
+   val.alg1 = VariableElimination(aiCovid)
+   alg1.start()
+   println("Probability of asimptomatic: "+alg1.probability(aiCovid, true))
+   alg1.kill
+ }      
 }
